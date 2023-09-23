53bb3cd53b237e77fb790fa3d80f82a0e7b3a275
Sajid <spareeth@gmail.com>
2023-09-23 13:22:18 +0200

    latest

--- data_ingest_v5.py
+++ data_ingest_v5.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-#!/usr/bin/env python3
+#!/usr/bin/python3
 '''
 Sajid Pareeth, 2023
 The script does post processing of fewsnet data based on countries
@@ -24,6 +23,7 @@ import grass.script as grass
 import grass.script.setup as gsetup
 from pathlib import Path
 import zipfile
+from sh import gunzip
 
 #data='viirs_ndvi'
 #inputfolder='/home/ubuntu/data/fewsnet'
@@ -32,77 +32,140 @@ import zipfile
 #General
 @click.argument('data')
 @click.argument('infolder')
+
 def main(data, infolder):
     """Ingest the newly downloaded fewsnet data into map database.
         
         Arguments:
         
-        data - Options are viirs_ndvi, viirs_ndviano
+        data - Options are viirs_ndvi, viirs_ndviano, chirps, tamsat_monthly, tamsat_daily
 
-        inputfolder - Add the full path to the folder were list of downloaded zip files are stored
-        
+        infolder - Add the full path to the folder where list of downloaded zip files are stored
+
     """
-    ## USER INPUTS ##
-    data = r"%s" %str(data)
-    gisdb = '/home/ubuntu/efs-mount/mapdata'
-    location = 'latlong'
-    if data == 'viirs_ndvi':
-        mapset = 'ndvi_viirs'
-    elif data == 'viirs_ndviano':
-        mapset = 'ndviano_viirs'
-    elif data == 'modis_ndvi':
-        mapset = 'ndvi_modis'
-    else:
-        mapset = 'ndviano_modis'
+    try:
+        ## USER INPUTS ##
+        data = r"%s" %str(data)
+        gisdb = '/home/ubuntu/s3-mount/mapdata'
+        location = 'latlong'
+        if data == 'viirs_ndvi':
+            mapset = 'ndvi_viirs'
+        elif data == 'viirs_ndviano':
+            mapset = 'ndviano_viirs'
+        elif data == 'modis_ndvi':
+            mapset = 'ndvi_modis'
+        elif data == 'modis_ndvi':
+            mapset = 'ndviano_modis'
+        elif data == 'tamsat_monthly':
+            mapset = 'tamsat'
+        elif data == 'tamsat_daily':
+            mapset = 'tamsat_daily'
+        elif data == 'chirps':
+            mapset = 'chirps'
+        else:
+            print('data is not supported')
+            return
 
-    print(mapset)
-    INDAT = r"%s" %str(infolder)
-    # Python path: we ask GRASS GIS where its Python packages are
-    sys.path.append(
-        subprocess.check_output(["grass", "--config", "python_path"], text=True).strip()
-    )
+        print(mapset)
+        INDAT = r"%s" %str(infolder)
+        # Python path: we ask GRASS GIS where its Python packages are
+        sys.path.append(
+            subprocess.check_output(["grass", "--config", "python_path"], text=True).strip()
+        )
 
-    # set some common environmental variables, like:
-    os.environ.update(dict(GRASS_COMPRESS_NULLS='1',
-                            GRASS_OVERWRITE='1',
-                            GRASS_COMPRESSOR='ZSTD'))
+        # set some common environmental variables, like:
+        os.environ.update(dict(GRASS_COMPRESS_NULLS='1',
+                                GRASS_OVERWRITE='1',
+                                GRASS_COMPRESSOR='ZSTD'))
 
-    #user = Session()
-    #user.open(gisdb=gisdb, location=location, mapset=mapset)
-    session = gsetup.init(gisdb, location, mapset)
+        #user = Session()
+        #user.open(gisdb=gisdb, location=location, mapset=mapset)
+        session = gsetup.init(gisdb, location, mapset)
 
-    s1="*.zip"
-    pt1=os.path.join(INDAT, s1)
-    listzip=glob.glob(pt1)
-    for dt1 in listzip:
-        with zipfile.ZipFile(dt1, 'r') as zip_ref:
-            zip_ref.extractall(INDAT)
-    print('Unzipping files')
+        if data == 'viirs_ndvi' or data == 'viirs_ndviano':
+            s1="*.zip"
+            pt1=os.path.join(INDAT, s1)
+            listzip=glob.glob(pt1)
+            for dt1 in listzip:
+                with zipfile.ZipFile(dt1, 'r') as zip_ref:
+                    zip_ref.extractall(INDAT)
+            print('Unzipping files')
 
-    s2="*.tif"
-    pt2=os.path.join(INDAT, s2)
-    listtif=glob.glob(pt2)
+            s2="*.tif"
+            pt2=os.path.join(INDAT, s2)
+            listtif=glob.glob(pt2)
 
-    for dt2 in listtif:
-        out1=os.path.basename(dt2)
-        #out2=out1.rsplit('.',1)[0]
-        yr = out1.rsplit('.',1)[0][2:][:2]
-        yy = '20'+yr
-        dt = out1.rsplit('.',1)[0][4:][:4]
-        out = mapset + '_' + yy + '_' + dt
-        r.in_gdal(input=dt2, output=out, overwrite=True)
-        ##grass.run_command("r.in.gdal", input=dt, output=out2, overwrite=True)
+            for dt2 in listtif:
+                out1=os.path.basename(dt2)
+                #out2=out1.rsplit('.',1)[0]
+                yr = out1.rsplit('.',1)[0][2:][:2]
+                yy = '20'+yr
+                dt = out1.rsplit('.',1)[0][4:][:4]
+                out = mapset + '_' + yy + '_' + dt
+                r.in_gdal(input=dt2, output=out, overwrite=True)
+                ##grass.run_command("r.in.gdal", input=dt, output=out2, overwrite=True)
+        else:
+            print('data is rainfall')
 
-    session.finish()
-    files = os.listdir(INDAT)
+        ## Data source for tamsat:
+        ## wget https://gws-access.jasmin.ac.uk/public/tamsat/rfe/data/v3.1/monthly/${i}/${m}/rfe${i}_${m}.v3.1.nc (i=year, m=month)
+        if data == 'tamsat_monthly':
+            s1="*.nc"
+            pt1=os.path.join(INDAT, s1)
+            listnc=glob.glob(pt1)
+            for dt2 in listnc:
+                out1=os.path.basename(dt2)
+                yr = out1.rsplit('.',3)[0][3:][:4]
+                mm = out1.rsplit('.',3)[0][8:][:9]
+                in1 = 'NETCDF' + ':"' + INDAT + '/rfe' + yr + '_' + mm + '.v3.1.nc":rfe_filled'
+                out = mapset + '_rfe' + yr + '_' + mm
+                r.in_gdal(input=in1, output=out, flags="o", overwrite=True)
 
-    for item in files:
-        if item.endswith(".tif"):
-            os.remove(os.path.join(INDAT, item))
+        if data == 'tamsat_daily':
+            s1="*.nc"
+            pt1=os.path.join(INDAT, s1)
+            listnc=glob.glob(pt1)
+            for dt2 in listnc:
+                out1=os.path.basename(dt2)
+                yr = out1.rsplit('.',3)[0][3:][:4]
+                mm = out1.rsplit('.',3)[0][8:10]
+                dd = out1.rsplit('.',3)[0][11:14]
+                in1 = 'NETCDF' + ':"' + INDAT + '/rfe' + yr + '_' + mm + '_'+ dd + '.v3.1.nc":rfe_filled'
+                out = mapset + '_rfe' + yr + '_' + mm + '_' + dd
+                r.in_gdal(input=in1, output=out, flags="o", overwrite=True)
+        
+        ## Data source for Chirps - # Chirps monthly: https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_monthly/tifs/
+        ## wget -r -np -R "index.html*" -e robots=off https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_monthly/tifs/
+        ## chirps-v2.0.2023.06.tif
+        if data == 'chirps':
+            s1="*.gz"
+            pt1=os.path.join(INDAT, s1)
+            listnc=glob.glob(pt1)
 
-    for item in files:
-        if item.endswith(".tfw"):
-            os.remove(os.path.join(INDAT, item))
+            for dt2 in listnc:
+                gunzip(dt2, "-f")
+                out1=os.path.basename(dt2)
+                yr = out1.rsplit('.',4)[1]
+                mm = out1.rsplit('.',4)[2]
+                in1 = out1.rsplit('.',1)[0]
+                in2 = INDAT + "/" + in1
+                out = 'chirps_monthly_' + yr + '_' + mm
+                r.in_gdal(input=in2, output=out, flags="o", overwrite=True)
 
+        session.finish()
+    finally:
+        files = os.listdir(INDAT)
+
+        for item in files:
+            if item.endswith(".tif"):
+                os.remove(os.path.join(INDAT, item))
+
+        for item in files:
+            if item.endswith(".tfw"):
+                os.remove(os.path.join(INDAT, item))
+        
+        for item in files:
+            if item.endswith(".nc"):
+                os.remove(os.path.join(INDAT, item))
 if __name__ == '__main__':
     main()
