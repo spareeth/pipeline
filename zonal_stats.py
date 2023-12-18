97aae6bea4d65c07ac17bfa00fd9e6e7e72d7a3a
Sajid <spareeth@gmail.com>
2023-12-18 16:55:01 +0100

    changed the scale (res) to 200-250m

--- zonal_stats.py
+++ zonal_stats.py
@@ -153,7 +153,7 @@ def main(sh, gr, cr, sg, bnd, data, method, outfolder, outcsv, start, end):
 
         #grass.run_command("v.import", input=infile, output="bnds", overwrite=True)
         #maps = ["ndvi_annual_" + s for s in years_str]
-        g.region(vector=bnd, res=0.003)
+        g.region(vector=bnd, res=0.002)
         grass.run_command('g.copy', vector=(bnd,'bnd'))
 
         if sh:
