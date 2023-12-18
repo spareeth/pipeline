9b80bfb62965908dd48561176d024b4cee504465
Sajid <spareeth@gmail.com>
2023-12-18 18:38:06 +0100

    output as multipolygon

--- zonal_stats.py
+++ zonal_stats.py
@@ -207,7 +207,7 @@ def main(sh, gr, cr, sg, bnd, data, method, outfolder, outcsv, start, end):
         #outfile="stats_" + str(a) + "_" + suffixcsv + ".csv"
         outfile=outcsv + ".csv"
         outfile1=os.path.join(outfolder, outfile)
-        v.out_ogr(input='bnd', output=outfile1, format='CSV')
+        v.out_ogr(input='bnd', output=outfile1, format='CSV', flags="m")
         user.close()
     finally:
         #####CLEANUP
