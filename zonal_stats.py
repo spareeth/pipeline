ea4ef1f1e3cfe7eaeda38124d08a41b0dd801f0f
Sajid <spareeth@gmail.com>
2024-01-09 23:50:29 +0100

    changed to bnd@boundaries

--- zonal_stats.py
+++ zonal_stats.py
@@ -155,7 +155,8 @@ def main(sh, gr, cr, sg, bnd, data, method, outfolder, outcsv, start, end):
         #grass.run_command("v.import", input=infile, output="bnds", overwrite=True)
         #maps = ["ndvi_annual_" + s for s in years_str]
         g.region(vector=bnd, res=0.0001)
-        grass.run_command('g.copy', vector=(bnd,'bnd'))
+        bound = bnd + '@boundaries'
+        grass.run_command('g.copy', vector=(bound,'bnd'))
 
         if sh:
             r.mask(raster="worldcover", maskcats='20')
